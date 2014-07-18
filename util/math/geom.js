dd80629aedf79557170a5101a05030a2e4d056b1
Karl Hellstern <khellste@andrew.cmu.edu>
2014-07-17 18:50:19 -0700

    Cleaned up some unused utilities

--- util/math/geom.js
+++ util/math/geom.js
@@ -6,28 +6,21 @@ sh.util = sh.util || {};
 sh.util.math = sh.util.math || {};
 sh.util.math.geom = sh.util.math.geom || {};
 
-sh.util.math.geom.projVert = function (a, b, x, ignoreBackwards) {
-	if (typeof ignoreBackwards !== 'boolean') {
-		ignoreBackwards = true;
-	}
+sh.util.math.geom.projVert = function (a, b, x) {
 	var pos = a, dir = { x: b.x - a.x, y: b.y - a.y };
 	if (dir.x === 0) return null;
 	var t = (x - pos.x)/dir.x;
-	if (ignoreBackwards && t < 0) return null;
-	return pos.y + t * dir.y;
+	return (t < 0) ? null : (pos.y + t * dir.y);
 };
 
-sh.util.math.geom.projHorz = function (a, b, y, ignoreBackwards) {
-	if (typeof ignoreBackwards !== 'boolean') {
-		ignoreBackwards = true;
-	}
+sh.util.math.geom.projHorz = function (a, b, y) {
 	var pos = a, dir = { x: b.x - a.x, y: b.y - a.y };
 	if (dir.y === 0) return null;
 	var t = (y - pos.y)/dir.y;
-	if (ignoreBackwards && t < 0) return null;
-	return pos.x + t * dir.x;
+	return (t < 0) ? null : (pos.x + t * dir.x);
 };
 
+// TODO Clean up this method to make fewer comparisons
 sh.util.math.geom.polyContains = function (points, test) {
 	var len = points.length, res = false;
 	for (var i = 0, j = len - 1; i < len; j = i++) {
